782509e75de844ce3966cd6d443ebaff4a17dd5a
jonathanwong110 <jonathan.wong110@gmail.com>
2020-12-10 14:01:06 -0500

    users can now add bookmarks

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameInput = document.getElementById('website-name');
+const websiteUrlInput = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
 let bookmarks = {}
 
 function displayBookmarks() {
-	// Remove all bookmark elements
-	bookmarksContainer.textContent = '';
-	// Build items
-	Object.keys(bookmarks).forEach((id) => {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = '';
+  // Build items
+  Object.keys(bookmarks).forEach((id) => {
+ 
+    const { name, url } = bookmarks[id];
 
-		const { name, url } = bookmarks[id];
-
-		// Item
-		const item = document.createElement('div');
-		item.classList.add('item');
-		// Close Icon
-		const closeIcon = document.createElement('i');
-		closeIcon.classList.add('fas', 'fa-times');
-		// Favicon / Link Container
-		const linkInfo = document.createElement('div');
-		linkInfo.classList.add('name');
-		// Favicon
-		const favicon = document.createElement('img');
-		favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
-		favicon.setAttribute('alt', 'Favicon');
-		// Link
-		const link = document.createElement('a');
-		link.setAttribute('href', `${url}`);
-		link.setAttribute('target', '_blank');
-		link.textContent = name;
-		// Append to bookmarks container
-		linkInfo.append(favicon, link);
-		item.append(closeIcon, linkInfo);
-		bookmarksContainer.appendChild(item);
-	});
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fas', 'fa-times');
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
 }
 
 function getBookmarks() {
@@ -48,5 +51,29 @@ function getBookmarks() {
   }
   displayBookmarks()
 }
+
+function addBookmark(e) {
+  e.preventDefault()
+  const nameValue = websiteNameInput.value;
+  let urlValue = websiteUrlInput.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  // Set bookmark object, add to array
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks[urlValue] = bookmark;
+  // Set bookmarks in localStorage, fetch, reset input fields
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  getBookmarks();
+  bookmarkForm.reset();
+  websiteNameInput.focus();
+}
+
+// Event Listener for Submitting
+
+bookmarkForm.addEventListener('submit', addBookmark);
 
 getBookmarks()
